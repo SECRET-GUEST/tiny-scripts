a841e13838b2600008133a6ccf5dab286907e054
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2022-12-21 12:39:35 +0100

    Update create a simple Wordlist.py

--- List files/create a simple Wordlist.py
+++ List files/create a simple Wordlist.py
@@ -1,6 +1,9 @@
 #Create a simple wordlist "1.png,....,500.png"
+#You have to change the path keeping the r before and finish par \YOURFILE.txt
+# (And have to create the YOURFILE.txt also for some reason)
 
-fichier = open("data.txt", "w+")
+fichier = open(
+    r"D:\...CHANGE THIS\data.txt", "w+")
 i = 1
 extension = ".png,"
 
