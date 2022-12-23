25ecb23aee8e9343b315704706418f20bd65404e
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2022-12-23 09:25:53 +0100

    Update FULLscreenYOUTUBE.py

--- Youtube full screen/FULLscreenYOUTUBE.py
+++ Youtube full screen/FULLscreenYOUTUBE.py
@@ -7,7 +7,7 @@ webbrowser.open(
     "https://www.youtube.com/channel/UCffuj1MEaD6Ga5beTRa525A"
     )
 
-#Wait 3s (for loading)
+#Wait 3s (for the page's loading) put less or more.
 time.sleep(3)
 
 #then press F key to fullscreen
