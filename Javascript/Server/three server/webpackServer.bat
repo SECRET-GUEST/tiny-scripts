f2a390dd9c70dfa4bf48aeb12f042718e543d3dd
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-06-20 09:00:47 +0200

    Update webpackServer.bat

--- Javascript/Server/three server/webpackServer.bat
+++ Javascript/Server/three server/webpackServer.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 
-
+:askLanguage
 echo (1) Francais
 echo (2) English
 SET /P language=Choisissez votre langue / Choose your language: 
@@ -19,19 +19,19 @@ IF "%language%"=="1" (
     SET "text9=Creation du fichier HTML..."
     SET "text10=Voulez-vous ouvrir Visual Studio Code (O/N) ?"
     SET "text11=Voulez-vous demarrer le serveur (O/N) ?"
-    SET "text12a=Votre configuration est presque terminée."
-    SET "text12b=Votre projet est maintenant prêt à être utilisé avec Webpack."
-    SET "text12c=Pour démarrer votre serveur, vous pouvez utiliser le fichier 'server.bat' créé lors de l'installation."
-    SET "text12d=Tous vos fichiers .js et dépendances doivent être placés dans le dossier 'src'. Webpack s'occupera automatiquement de la compilation et du bundling lors de l'exécution du serveur."
+    SET "text12a=Votre configuration est presque terminee."
+    SET "text12b=Votre projet est maintenant pret à etre utilise avec Webpack."
+    SET "text12c=Pour demarrer votre serveur, vous pouvez utiliser le fichier 'server.bat' cree lors de l'installation."
+    SET "text12d=Tous vos fichiers .js et dependances doivent etre places dans le dossier 'src'. Webpack s'occupera automatiquement de la compilation et du bundling lors de l'execution du serveur."
     SET "text12e=Appuyez sur entrer pour continuer"
     SET "text13=Voulez-vous creer un fichier CSS de base (O/N) ?"
     SET "text14=Creation du fichier CSS..."
     SET "text15=/* Ajoutez votre code CSS ici */"
     SET "text16=// Ajoutez votre code Three.js ici"
-    SET "text17=echo 1. Démarrer le serveur en mode développement"
-    SET "text18=echo - En mode développement, Webpack optimise la vitesse de construction et ajoute des outils pour faciliter le débogage. Il ne réduit pas le code, inclut des sources cartographiques pour faciliter le débogage et ne supprime pas certains avertissements et erreurs de développement."
-    SET "text19=echo 2. Démarrer le serveur en mode production"
-    SET "text20=echo - En mode production, Webpack optimise la taille finale du bundle et les performances d'exécution. Il réduit le code, supprime le code inutilisé, effectue des optimisations de performance et désactive certains outils de débogage pour réduire la taille du bundle."
+    SET "text17=echo 1. Demarrer le serveur en mode developpement"
+    SET "text18=echo - En mode developpement, Webpack optimise la vitesse de construction et ajoute des outils pour faciliter le debogage. Il ne reduit pas le code, inclut des sources cartographiques pour faciliter le debogage et ne supprime pas certains avertissements et erreurs de developpement."
+    SET "text19=echo 2. Demarrer le serveur en mode production"
+    SET "text20=echo - En mode production, Webpack optimise la taille finale du bundle et les performances d'execution. Il reduit le code, supprime le code inutilise, effectue des optimisations de performance et desactive certains outils de debogage pour reduire la taille du bundle."
     SET "htmlLang=fr"
 ) ELSE IF "%language%"=="2" (
     SET "text1=Enter the name of your project: "
@@ -59,7 +59,12 @@ IF "%language%"=="1" (
     SET "text19=echo 2. Start server in production mode "
     SET "text20=echo    - In production mode, Webpack optimizes for the final bundle size and runtime performance. It minimizes the code, removes dead code, performs performance optimizations, and disables some debugging tools to reduce the bundle size."
     SET "htmlLang=en"
+) ELSE (
+  cls
+  GOTO askLanguage
 )
+:setLanguage
+
 
 SET /P projectName=!text1!
 echo !text2!
@@ -68,18 +73,57 @@ cls
 echo !text3!
 mkdir !projectName!
 cd !projectName!
-call npm init -y
-call npm install --save three
-call npm install --save-dev vite webpack webpack-cli
+
+echo Creating package.json
+(
+echo {
+echo   "name": "!projectName!",
+echo   "version": "1.0.0",
+echo   "description": "",
+echo   "main": "index.js",
+echo   "scripts": {
+echo     "start": "webpack serve --open",
+echo     "build": "webpack"
+echo   },
+echo   "keywords": [],
+echo   "author": "",
+echo   "license": "ISC",
+echo   "dependencies": {
+echo     "three": "^0.153.0"
+echo   },
+echo   "devDependencies": {
+echo     "@babel/core": "^7.22.5",
+echo     "@babel/preset-env": "^7.22.5",
+echo     "babel-loader": "^9.1.2",
+echo     "css-loader": "^6.8.1",
+echo     "html-webpack-plugin": "^5.5.3",
+echo     "mini-css-extract-plugin": "^2.7.6",
+echo     "webpack": "^5.87.0",
+echo     "webpack-cli": "^5.1.4",
+echo     "webpack-dev-server": "^4.15.1"
+echo   }
+echo }
+) > package.json
+
+echo Installing dependencies
+call npm install
+
+
+
 cls
 echo !text4!
 echo const path = require('path');>> webpack.config.js
+echo const HtmlWebpackPlugin = require('html-webpack-plugin');>> webpack.config.js
+echo const MiniCssExtractPlugin = require('mini-css-extract-plugin');>> webpack.config.js
 echo.>> webpack.config.js
 echo module.exports = {>> webpack.config.js
 echo   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',>> webpack.config.js
-echo   entry: './src/index.js',>> webpack.config.js
+echo   entry: {>> webpack.config.js
+echo     main: './src/index.js',>> webpack.config.js
+echo     style: './src/style.css',>> webpack.config.js
+echo   },>> webpack.config.js
 echo   output: {>> webpack.config.js
-echo     filename: 'main.js',>> webpack.config.js
+echo     filename: '[name].js',>> webpack.config.js
 echo     path: path.resolve(__dirname, 'dist'),>> webpack.config.js
 echo   },>> webpack.config.js
 echo   module: {>> webpack.config.js
@@ -93,7 +137,15 @@ echo           options: {>> webpack.config.js
 echo             presets: ['@babel/preset-env']>> webpack.config.js
 echo           }>> webpack.config.js
 echo         }>> webpack.config.js
-echo       }>> webpack.config.js
+echo       },>> webpack.config.js
+echo       {>> webpack.config.js
+echo         test: /\.css$/,>> webpack.config.js
+echo         use: [MiniCssExtractPlugin.loader, 'css-loader'],>> webpack.config.js
+echo       },>> webpack.config.js
+echo       {>> webpack.config.js
+echo         test: /\.(png^|svg^|jpg^|jpeg^|gif)$/i,>> webpack.config.js
+echo         type: 'asset/resource',>> webpack.config.js
+echo       },>> webpack.config.js
 echo     ]>> webpack.config.js
 echo   },>> webpack.config.js
 echo   resolve: {>> webpack.config.js
@@ -102,6 +154,16 @@ echo       'three': path.resolve(__dirname, 'node_modules/three/build/three.modu
 echo       'GLTFLoader': path.resolve(__dirname, 'node_modules/three/examples/jsm/loaders/GLTFLoader.js')>> webpack.config.js
 echo     }>> webpack.config.js
 echo   },>> webpack.config.js
+echo   plugins: [>> webpack.config.js
+echo     new HtmlWebpackPlugin({>> webpack.config.js
+echo       template: path.resolve(__dirname,'index.html'),>> webpack.config.js
+echo       filename: 'index.html',>> webpack.config.js
+echo       inject: 'body',>> webpack.config.js
+echo     }),>> webpack.config.js
+echo     new MiniCssExtractPlugin({>> webpack.config.js
+echo       filename: 'style.css',>> webpack.config.js
+echo     }),>> webpack.config.js
+echo   ],>> webpack.config.js
 echo   devServer: {>> webpack.config.js
 echo     static: {>> webpack.config.js
 echo       directory: path.join(__dirname, 'dist'),>> webpack.config.js
@@ -115,6 +177,7 @@ echo };>> webpack.config.js
 cls
 echo !text5!
 mkdir src
+mkdir src\assets
 echo !text16! >> src\index.js
 cls
 echo !text6!
@@ -155,30 +218,42 @@ echo echo Invalid option >> server.bat
 echo timeout /t 2 ^>nul >> server.bat
 echo goto menu >> server.bat
 cls
+:askHtml
 SET /P createHtml=!text8!
 IF /I "!createHtml!" EQU "O" (
     echo !text9!
-    echo ^<!DOCTYPE html^>> index.html
-    echo ^<html lang="!htmlLang!"^>>> index.html
-    echo ^<head^>>> index.html
-    echo     ^<meta charset="UTF-8"^>>> index.html
-    echo     ^<title^>!projectName!^</title^>>> index.html
-    echo     ^<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://code.jquery.com; img-src 'self' https://*.svg https://*.png https://*.jpg; child-src 'self' https://www.youtube.com; style-src 'self'"^>>> index.html
-    echo     ^<link rel="stylesheet" type="text/css" href="style/style.css"^>>> index.html
-    echo ^</head^>>> index.html
-    echo ^<body^>>> index.html
-    echo     ^<script src="dist/main.js"^>^</script^>>> index.html
-    echo ^</body^>>> index.html
-    echo ^</html^>>> index.html
+    echo ^<!DOCTYPE html^> > index.html
+    echo ^<html lang="!htmlLang!"^> >> index.html
+    echo ^<head^> >> index.html
+    echo     ^<meta charset="UTF-8"^> >> index.html
+    echo     ^<title^>!projectName!^</title^> >> index.html
+    echo     ^<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://code.jquery.com; img-src 'self' https://*.svg https://*.png https://*.jpg; child-src 'self' https://www.youtube.com; style-src 'self'"^> >> index.html
+    echo     ^<link rel="stylesheet" type="text/css" href="style.css"^> >> index.html
+    echo ^</head^> >> index.html
+    echo ^<body^> >> index.html
+    echo     ^<script src="dist/main.js"^>^</script^> >> index.html
+    echo ^</body^> >> index.html
+    echo ^</html^> >> index.html
     cls
+    :askCss
     SET /P createCss=!text13!
     IF /I "!createCss!" EQU "O" (
         echo !text14!
-        mkdir style
-        echo !text15! >> style\style.css
+        echo !text15! > style.css
         cls
+    ) ELSE IF /I "!createCss!" EQU "N" (
+        cls
+    ) ELSE (
+      cls
+      GOTO askCss
     )
+) ELSE IF /I "!createHtml!" EQU "N" (
+    cls
+) ELSE (
+  cls
+  GOTO askHtml
 )
+
 cls
 echo !text12a!
 echo !text12b!
@@ -186,13 +261,25 @@ echo !text12c!
 echo !text12d!
 echo !text12e!
 pause > nul
+:askCode
 SET /P openVSCode=!text10!
 IF /I "!openVSCode!" EQU "O" (
     START code .
+) ELSE IF /I "!openVSCode!" EQU "N" (
+    cls
+) ELSE (
+  cls
+  GOTO askCode
 )
+
+:askServer
 SET /P startServer=!text11!
 IF /I "!startServer!" EQU "O" (
     call server.bat
+) ELSE IF /I "!startServer!" EQU "N" (
+    cls
+) ELSE (
+  cls
+  GOTO askServer
 )
-
 
