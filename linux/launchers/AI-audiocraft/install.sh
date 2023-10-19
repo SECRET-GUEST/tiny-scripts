536b256a191a96cb7f6b5ea25b207ce3acce28df
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-10-20 00:17:58 +0200

    Update install.sh

--- linux/launchers/AI-audiocraft/install.sh
+++ linux/launchers/AI-audiocraft/install.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
 
-# Define the path to the dependencies folder
-DEPENDENCIES_DIR=dependencies
+# Get the path of the directory containing this script
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+# Set the path to the dependencies folder
+DEPENDENCIES_DIR="$SCRIPT_DIR/dependencies"
 
 # Create the dependencies folder if it does not exist
-mkdir -p $DEPENDENCIES_DIR
+mkdir -p "$DEPENDENCIES_DIR"
 
-# Download Miniconda if it has not been downloaded yet
+# Download Miniconda if it hasn't been downloaded yet
 if [[ ! -f "$DEPENDENCIES_DIR/Miniconda3-latest-Linux-x86_64.sh" ]]; then
-    wget -P $DEPENDENCIES_DIR https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    wget -P "$DEPENDENCIES_DIR" https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 fi
 
 # Install Miniconda if necessary
 if ! command -v conda &>/dev/null; then
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p $PWD/miniconda
-    export PATH="$PWD/miniconda/bin:$PATH"
+    bash "$DEPENDENCIES_DIR/Miniconda3-latest-Linux-x86_64.sh" -b -p "$DEPENDENCIES_DIR/miniconda"
+    export PATH="$DEPENDENCIES_DIR/miniconda/bin:$PATH"
 fi
 
 # Activate Conda
-source miniconda/bin/activate
+source "$DEPENDENCIES_DIR/miniconda/bin/activate"
 
-# Check if Conda is activated
-if ! command -v conda &>/dev/null; then
-    echo "The virtual environment has not started. Stopping the script."
+# Create the Conda environment if it does not exist
+ENV_NAME="venv"  # Change this to the environment name you want
+if ! conda info --envs | grep -q "$ENV_NAME"; then
+    conda create -p "$DEPENDENCIES_DIR/$ENV_NAME" python=3.9 -y
+fi
+
+# Activate the environment my_env
+conda activate "$DEPENDENCIES_DIR/$ENV_NAME"
+
+# Check if the environment my_env is activated
+if [[ $CONDA_DEFAULT_ENV != "$DEPENDENCIES_DIR/$ENV_NAME" ]]; then
+    echo "The virtual environment $ENV_NAME is not activated. Stopping the script."
     exit 1
 fi
 
-# Create the Conda environment if it does not exist
-if ! conda info --envs | grep -q 'audiocraft'; then
-    conda create -n audiocraft python=3.9 -y
-fi
-
-# Activate the audiocraft environment
-conda activate audiocraft
+# Update typing_extensions
+pip install --upgrade typing_extensions
 
 # Clone the audiocraft repository if it does not exist
-if [[ ! -d "audiocraft" ]]; then
-    git clone https://github.com/facebookresearch/audiocraft.git
-    cd audiocraft/
+REPO_NAME="audiocraft"  # Change this to the directory name you want
+if [[ ! -d "$SCRIPT_DIR/$REPO_NAME" ]]; then
+    git clone https://github.com/facebookresearch/audiocraft.git "$SCRIPT_DIR/$REPO_NAME"
+    cd "$SCRIPT_DIR/$REPO_NAME/"
     python -m pip install -r requirements.txt
 else
-    cd audiocraft/
+    cd "$SCRIPT_DIR/$REPO_NAME/"
 fi
 
-# Launch the application
-python -m demos.musicgen_app
+# Launch the application in a background process
+python -m demos.musicgen_app --inbrowser
